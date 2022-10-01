8b3db4dfc78005b12063f0f2c78144a206f10ea8
loudbel <amine.beloud@gmail.com>
2022-10-01 18:40:07 +0100

    thunk added

--- src/components/MovieList3.js
+++ src/components/MovieList3.js
@@ -1,10 +1,17 @@
-import { useSelector } from "react-redux";
-import { useState } from 'react';
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect, useState } from 'react';
 import Filter from "./Filter";
+import { fetchMovies } from "../redux/features/movieSlice";
+
 function MovieList3() {
   const movielist = useSelector((state) => state.movie.movies);
   console.log(movielist);
   const [FiltredList, setFiltredListe] = useState(movielist);
+  const dispatch = useDispatch();
+
+  useEffect(()=>{
+    setFiltredListe(movielist)
+  },[movielist])
 
   const handlefilter = (title) => {
     console.log("handlefilterdlist:::::",title);
@@ -14,6 +21,10 @@ function MovieList3() {
       })
     );
   };
+  const handleReloadClick = ()=>{
+    console.log('get data')
+    dispatch(fetchMovies())
+  }
 
   return (
     <div>
@@ -23,6 +34,7 @@ function MovieList3() {
           <li key={i}>{m.title}</li>
         ))}
       </ul>
+      <button onClick={handleReloadClick}>GET DATA</button>
     </div>
   );
 }
--- src/redux/features/movieSlice.js
+++ src/redux/features/movieSlice.js
@@ -1,8 +1,34 @@
-import { createSlice } from "@reduxjs/toolkit";
-
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from 'axios';
 import {moviesList} from "../../assets/mookData"
 
-const initialState= {movies:moviesList}
+export const fetchMovies = createAsyncThunk("movie/fetchMovies", ()=>{
+ return axios('https://www.omdbapi.com/?apikey=edf8783e&r=json&type=movie&s=bat')
+    .then((response)=>{
+        return response.data.Response == 'True' ? response.data.Search.map((m)=>
+            {
+                return{
+                    title: m.Title,
+                    poster: m.Poster,
+                    description: `The aging patriarch of an organized crime 
+                    dynasty transfers control of his clandestine 
+                    empire to his reluctant son.`,
+                    rate: 5
+                }
+
+            }
+        ) : []
+
+    })
+})
+
+//https://www.omdbapi.com/?apikey=edf8783e&r=json&type=movie&s=bat
+
+const initialState= {
+    movies:moviesList,
+    loading: false,
+    error: ''
+}
 
 const movieSlice= createSlice({
     name:"movie",
@@ -11,6 +37,24 @@ const movieSlice= createSlice({
         addmovie:(state,action)=>{
             state.movies.push(action.payload)
         }
+    },
+    extraReducers: (builder)=>{
+        builder.addCase(fetchMovies.pending, (state)=>{
+            state.movies = []
+            state.loading = true
+            state.error =  ''
+        })
+        builder.addCase(fetchMovies.fulfilled, (state, action)=>{
+            state.movies = action.payload
+            state.loading = false
+            state.error =  ''
+            console.log('fulfilllll', action.payload)
+        })
+        builder.addCase(fetchMovies.rejected, (state, action)=>{
+            state.movies = []
+            state.loading = false
+            state.error =  action.payload
+        })
     }
 
 })
